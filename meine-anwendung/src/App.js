e2032647b830918b67dd7411116cbeae4466901f
Ella G <elviraangelinagraf@gmail.com>
2024-03-08 21:30:06 +0100

    Added the html

--- meine-anwendung/src/App.js
+++ meine-anwendung/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Weather from "./Weather.js";
+import Atropos from "https://cdn.jsdelivr.net/npm/atropos@2/atropos.min.mjs";
 
+import "atropos/css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        Weather App
+      <div className="container atropos my-atropos">
+        <Weather />
         <footer>
           <p>
             This project was coded by{" "}
